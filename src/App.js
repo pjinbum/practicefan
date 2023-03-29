3ae0647d7799affd2d3136086d0519919c8fe18e
Insight <119594529+pjinbum@users.noreply.github.com>
2023-03-29 10:08:43 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Gesifan from './Gesifan/Gesifan';
+
+function App() {
+  return (
+    <div className="App">
+      <Gesifan></Gesifan>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Gesifan/Gesifan.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import BoardCreate from './BoardCreate'
+import BoardList from './BoardList'
+
+const Gesifan = () => {
+  return (
+    <div>
+      <BoardCreate></BoardCreate>
+    </div>
+  )
+}
+
+export default Gesifan
